e4ef9e31c3d8b6ef87df1f7f932f6be87d8e73f7
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2024-01-03 18:46:38 -0300

    changes

--- project-back/db/config.js
+++ project-back/db/config.js
@@ -1,8 +1,11 @@
+
+import 'dotenv/config'
+
 export const config = {
-    host: 'localhost',
-    port: 3306, 
-    user:'root',
-    password: 'pin420608',
-    database: 'proyecto1'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 }
    
--- project-back/index.js
+++ project-back/index.js
@@ -1,16 +1,3 @@
-/*
-
-const app = express();
-
-const PORT = process.env.PORT || 3000
-
-app.use(AppRoutes);
-app.use(express.json());
-app.use(cors());
-app.listen(PORT);ds
-
-
-*/
 
 import  express  from "express";
 import AppRoutes from './routes/AppRoutes.js'
@@ -22,7 +9,8 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use(AppRoutes);
+const PORT = process.env.PORT || 3000
 app.listen(process.env.PORT || 3000);
 
 
-console.log('server on port 4000')
+console.log(`server on port ${PORT}`)
