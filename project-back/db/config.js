d86930359c267c6a8db3e5906a1e23d3db1bf92f
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2024-01-08 20:14:26 -0300

    credentials

--- project-back/db/config.js
+++ project-back/db/config.js
@@ -1,11 +1,10 @@
-
 import 'dotenv/config'
 
 export const config = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT, 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: 'localhost',
+    port: 3306, 
+    user:'root',
+    password: 'pin420608',
+    database: 'proyecto1'
 }
    
