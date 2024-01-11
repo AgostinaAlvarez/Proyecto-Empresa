dc9a41fe6f978ec76c6276b01bd0a3e02bf7c573
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2024-01-10 23:27:00 -0300

    afip

--- project-back/AFIP/afipConfig.js
+++ project-back/AFIP/afipConfig.js
@@ -1,16 +1,19 @@
+
 import fs from 'fs';
 import { Wsaa,Wsfe } from 'afipjs';
 import 'dotenv/config';
 
-const pem = fs.readFileSync(process.env.APP_ROUTE_CERT, 'utf8')
+const pem = fs.readFileSync(process.env.APP_ROUTE_CERT, 'utf8') 
 const key = fs.readFileSync(process.env.APP_ROUTE_KEY, 'utf8')
 
 const afipConfig = {
-    prod: false,
-    debug: true,
+    prod: false,  // indica si se va a utilizar el entorno de Produccion o el de Homologacion
+    debug: true, // indica si se deben mostrar datos de ejecucion
 }
 
+
 const wsaa = new Wsaa(afipConfig)
+
 
 wsaa.setCertificate(pem)
 wsaa.setKey(key)
